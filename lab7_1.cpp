da046e338630f56c95d289bb2992cc64ddc758ea
Pbirelay <zhoglisimp50@gmail.com>
2025-12-16 13:31:57 +0700

    lab7_1 complete

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
-int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+using namespace std;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+int main() {
+    double a  =  20; 
+    double b = 30;
+    double c = 4;
+    double output;
+    output = (a + b)/c;
+    cout << "output: " << output << "\n";
     return 0;
 }
